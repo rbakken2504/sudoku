ea25d14fa2c9b42d8c05c93c89fdfcf7eb3209cd
rbakken2504 <rbakken2504@yahoo.com>
2015-01-05 02:45:23 -0600

    add functionality to solve harder puzzles (not finished)

--- sudoku.rb
+++ sudoku.rb
@@ -3,17 +3,30 @@ class Sudoku
   @empty_cells_possible_vals = []
   @empty_cell_locations = []
   @empty_cells = 0
+  @empty_cells_hash = Hash.new
   @possibleVals = [1,2,3,4,5,6,7,8,9]
 
-  row1 = [7,9,6," ", " ", " ", 3, " ", " "]
-  row2 = [" "," "," "," ", 7, 6, 9, " ", " "]
-  row3 = [8," ", " ", " ", 3, " ", " ", 7, 6]
-  row4 = [" ", " ", " ", " ", " ", 5, " ", " ", 2]
-  row5 = [" ", " ", 5, 4,1,8,7," ", " "]
-  row6 = [4, " ", " ", 7, " ", " ", " ", " ", " "]
-  row7 = [6,1," ", " ", 9, " ", " ", " ", 8]
-  row8 = [5, " ", 2, 3, " ", " ", " ", " ", " "]
-  row9 = [3, " ", 9, " ", " ", " ", " ", 5, 4]
+  #Initialize Hash so I can push array into subgrid locations
+  @subgrid_hash = Hash.new
+  @subgrid_hash["1"] = []
+  @subgrid_hash["2"] = []
+  @subgrid_hash["3"] = []
+  @subgrid_hash["4"] = []
+  @subgrid_hash["5"] = []
+  @subgrid_hash["6"] = []
+  @subgrid_hash["7"] = []
+  @subgrid_hash["8"] = []
+  @subgrid_hash["9"] = []
+
+  row1 = [" ", " ", 5, " ", 9, " ", " ", " ", 1]
+  row2 = [" ", " ", " ", " ", " ", 2, " ", 7, 3]
+  row3 = [7, 6, " ", " ", " ", 8, 2, " ", " "]
+  row4 = [" ", 1, 2, " ", " ", 9, " ", " ", 4]
+  row5 = [" ", " ", " ", 2, " ", 3, " ", " ", " "]
+  row6 = [3, " ", " ", 1, " ", " ", 9, 6, " "]
+  row7 = [" ", " ", 1, 9, " ", " ", " ", 5, 8]
+  row8 = [9, 7, " ", 5, " ", " ", " ", " ", " "]
+  row9 = [5, " ", " ", " ", 3, " ", 7, " ", " "]
   grid = [row1, row2, row3,
           row4, row5, row6,
           row7, row8, row9]
@@ -28,10 +41,10 @@ class Sudoku
   end
 
   def self.get_index_of_empty(grid)
-    returnVals = []
     if grid == nil
       return []
     else
+      @empty_cell_locations.clear
       (0..8).each do |row|
         (0..8).each do |column|
           if(grid[row][column] == " ")
@@ -86,39 +99,174 @@ class Sudoku
     return possible_cell_vals
   end
 
-  def self.possible_values_for_all_cells(grid)
-    get_index_of_empty(grid)
-    (0..@empty_cell_locations.count - 1).each do |i|
-      row = @empty_cell_locations[i][0]
-      column = @empty_cell_locations[i][1]
-      @empty_cells_possible_vals.push([row, column, possible_values_for_cell(grid, row, column)])
+  def self.determine_subgrid(local_row, local_column)
+    subgrid = 0
+    if(local_row == 0 && local_column == 0)
+      subgrid = 1
+    elsif(local_row == 0 && local_column == 3)
+      subgrid = 2
+    elsif(local_row == 0)
+      subgrid = 3
+    elsif(local_row == 3 && local_column == 0)
+      subgrid = 4
+    elsif(local_row == 3 && local_column == 3)
+      subgrid = 5
+    elsif(local_row == 3)
+      subgrid = 6
+    elsif(local_row == 6 && local_column == 0)
+      subgrid = 7
+    elsif(local_row == 6 && local_column == 3)
+      subgrid = 8
+    else
+      subgrid = 9
+    end
+    return subgrid
+  end
+
+  def self.define_subgrid_location(row, column)
+    subgrid = 0
+    local_row    = grid_helper(row)
+    local_column = grid_helper(column)
+    subgrid = determine_subgrid(local_row, local_column)
+    return subgrid
+  end
+
+  def self.eliminate_possible_values_grid
+    subgrid = 1
+    while(subgrid < 10) do
+      cell = 0
+      while(cell < @subgrid_hash["#{subgrid}"].count) do
+        cell_to_check = 0
+        cur_row = @subgrid_hash["#{subgrid}"][cell][0]
+        cur_column = @subgrid_hash["#{subgrid}"][cell][1]
+        cur_vals = @subgrid_hash["#{subgrid}"][cell][2]
+        while(cell_to_check < @subgrid_hash["#{subgrid}"].count) do
+          if(@subgrid_hash["#{subgrid}"][cell_to_check] ==
+             @subgrid_hash["#{subgrid}"][cell])
+            #do nothing
+          else
+            cur_vals = cur_vals - @subgrid_hash["#{subgrid}"][cell_to_check][2]
+          end
+          cell_to_check = cell_to_check + 1
+        end
+        if(!cur_vals.empty?)
+          @empty_cells_hash["#{cur_row}#{cur_column}"] = cur_vals
+        end
+        cell = cell + 1
+      end
+      subgrid = subgrid + 1
     end
   end
 
-  def self.solve_sudoku(grid)
+  def self.possible_values_for_all_cells(grid)
+    get_index_of_empty(grid)
+    (1..9).each do |s_grid|
+      @subgrid_hash["#{s_grid}"].clear
+    end
+    (0..@empty_cell_locations.count - 1).each do |i|
+      row = @empty_cell_locations[i][0]
+      column = @empty_cell_locations[i][1]
+      subgrid = define_subgrid_location(row, column)
+      possible_values_cell = possible_values_for_cell(grid, row, column)
+      @empty_cells_hash["#{row}#{column}"] = possible_values_cell
+      #@empty_cells_possible_vals.push([row, column, subgrid, possible_values_cell])
+      @subgrid_hash["#{subgrid}"].push([row, column, possible_values_cell])
+    end
+    eliminate_possible_values_grid
+    eliminate_possible_values_column
+  end
+
+  def self.eliminate_possible_values_column
     i = 0
-    while( i < @empty_cells_possible_vals.count ) do
-      if(@empty_cells_possible_vals[i][2].count == 1)
-        row = @empty_cells_possible_vals[i][0]
-        column = @empty_cells_possible_vals[i][1]
-        possible_value = @empty_cells_possible_vals[i][2][0]
-        grid[row][column] = possible_value
-        @empty_cells_possible_vals.delete_at(i)
+    while(i < @empty_cell_locations.count) do
+      row = @empty_cell_locations[i][0]
+      column = @empty_cell_locations[i][1]
+      current_cell_values = @empty_cells_hash["#{row}#{column}"]
+      j = 0
+      ##COLUMNS
+      while(j < @empty_cell_locations.count) do
+        other_row = @empty_cell_locations[j][0]
+        other_column = @empty_cell_locations[j][1]
+        if(other_row != row && other_column == column)
+          other_cell_vals = @empty_cells_hash["#{other_row}#{other_column}"]
+          current_cell_values = current_cell_values - other_cell_vals
+        end
+        j = j + 1
+      end
+      if(!current_cell_values.empty?)
+        @empty_cells_hash["#{row}#{column}"] = current_cell_values
+      end
+      j = 0
+      ##ROWS
+      while(j < @empty_cell_locations.count) do
+        other_row = @empty_cell_locations[j][0]
+        other_column = @empty_cell_locations[j][1]
+        if(other_row == row && other_column != column)
+          other_cell_vals = @empty_cells_hash["#{other_row}#{other_column}"]
+          current_cell_values = current_cell_values - other_cell_vals
+        end
+        j = j + 1
+      end
+      if(!current_cell_values.empty?)
+        @empty_cells_hash["#{row}#{column}"] = current_cell_values
+      end
+
+      i = i + 1
+    end
+  end
+
+  def self.solve_empty_cells(grid)
+    i = 0
+    while( i < @empty_cell_locations.count ) do
+      row = @empty_cell_locations[i][0]
+      column = @empty_cell_locations[i][1]
+      if(@empty_cells_hash["#{row}#{column}"].count == 1)
+        possible_value = @empty_cells_hash["#{row}#{column}"]
+        grid[row][column] = possible_value[0]
+        @empty_cell_locations.delete_at(i)
+        @empty_cells_hash.delete("#{row}#{column}")
         @empty_cells = @empty_cells - 1
       end
       i = i + 1
     end
     print_grid(grid)
     puts "----------------------------"
+    puts "EMPTY CELLS HASH: "
+    print @empty_cells_hash
+    puts
+    #puts "EMPTY LOCATIONS:"
+    #print @empty_cell_locations
+    #puts
+    #puts "SUBGRID HASH"
+    #print @subgrid_hash
+    #puts
   end
 
+  def self.min_count_of_values()
+    min_value = 9
+    (0..@empty_cell_locations.count - 1).each do |i|
+      row = @empty_cell_locations[i][0]
+      col = @empty_cell_locations[i][1]
+      cell_values = @empty_cells_hash["#{row}#{col}"]
+      if(cell_values.count < min_value)
+        min_value = cell_values.count
+      end
+    end
+    return min_value
+  end
 
-  possible_values_for_all_cells(grid)
-  #print_grid(grid)
-  #puts "------------------------------"
-  while(@empty_cells >= 0) do
-    solve_sudoku(grid)
+  def self.solve_sudoku(grid)
     possible_values_for_all_cells(grid)
+    while(@empty_cells >= 0) do
+      solve_empty_cells(grid)
+      possible_values_for_all_cells(grid)
+      min_cell_val_count = min_count_of_values
+      if(min_cell_val_count > 1)
+        #Handle case where two vals or more only exist
+      end
+    end
   end
+
+  solve_sudoku(grid)
 
 end
